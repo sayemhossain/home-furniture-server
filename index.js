d5d2f49ca674c282f8b04214fde32740aefdea05
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-05-05 15:18:54 +0600

    database connection done

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-//username: homeFurniture
-//pass: AcERklDKjeQ6zSnZ
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//username: dbuser
+//pass: TDMuDLwEUALynvoG
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // from mongodb
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const uri =
-  "mongodb+srv://homeFurniture:<password>@cluster0.ihsnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://dbuser:TDMuDLwEUALynvoG@cluster0.ihsnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("DB connected");
+    const furniturecollection = client
+      .db("homeFurniture")
+      .collection("furniture");
+
+    // get all furniture from database
+    app.get("/furnitures", async (req, res) => {
+      const query = {};
+      const cursor = furniturecollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   console.log("conndeddafdsfasdf");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 // from mongodb end
 
